bd0cb69f47f97b6b1de949b7e5f1eb9868eeed7d
Stefan Lindström <c12slm@cs.umu.se>
2015-10-02 16:59:51 +0200

    Fixed to support middleware changes.

--- src/remote/interfaces/NameServerInterface.java
+++ src/remote/interfaces/NameServerInterface.java
@@ -1,18 +1,14 @@
 package remote.interfaces;
 
-import remote.objects.ComModuleImp;
-
 import java.io.Serializable;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface NameServerInterface extends Serializable, Remote {
 
-    public void createGroup(String groupName, ComModuleInterface leader) throws RemoteException;
+    public ComModuleInterface joinGroup(String groupName, ComModuleInterface leader) throws RemoteException;
 
-    public ComModuleInterface joinGroup(String groupName) throws RemoteException;
-
-    public String[] groupList() throws RemoteException;
+    public String[][] groupList() throws RemoteException;
 
     public void assignNewLeader(String groupName, ComModuleInterface newLeader) throws RemoteException;
 
