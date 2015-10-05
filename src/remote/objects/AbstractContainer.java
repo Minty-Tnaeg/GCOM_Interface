c52f73f64e48344b8d5fb73a29793a853b9de7ab
Stefan Lindström <c12slm@cs.umu.se>
2015-10-05 14:55:23 +0200

    Added support for containers.

---
+++ src/remote/objects/AbstractContainer.java
@@ -0,0 +1,24 @@
+package remote.objects;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * Created by c12slm on 2015-10-05.
+ */
+public abstract class AbstractContainer {
+    private AbstractMessage message;
+
+    public AbstractContainer(AbstractMessage message) {
+        this.message = message;
+    }
+
+    public AbstractMessage getMessage(){
+        return this.message;
+    }
+
+    abstract public boolean isDeliverable(HashMap<UUID, Integer> vectorClock);
+
+    abstract public boolean isRepeat(HashMap<UUID, Integer> vectorClock);
+}
